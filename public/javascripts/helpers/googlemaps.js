7dd48b837c19c64303da61c07e67d8255cc16849
Maurin <maurin.lenglart@gmail.com>
2012-10-29 01:16:56 +0100

    online offline and maps

--- public/javascripts/helpers/googlemaps.js
+++ public/javascripts/helpers/googlemaps.js
@@ -5,7 +5,7 @@ define([
     // Fonction de callback en cas d’erreur
     function erreurPosition(error) {
         var info = "Erreur lors de la géolocalisation : ";
-        switch(error.code) {
+        switch (error.code) {
             case error.TIMEOUT:
                 info += "Timeout !";
                 break;
@@ -21,55 +21,62 @@ define([
         }
     }
 
+    function codeLatLng(lat, lng) {
+        geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(lat, lng);
+        geocoder.geocode({'latLng':latlng}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                    $('#inputPlace').val(results[5].address_components[0].long_name);
+            }
+        });
+    }
 
 
-
-    var app = {
-        init:function () {
-            var centerpos = new google.maps.LatLng(48.579400,7.7519);
+                    var app = {
+                        init:function () {
+                            var centerpos = new google.maps.LatLng(48.579400, 7.7519);
 
 // Ansi que des options pour la carte, centrée sur latlng
-            var optionsGmaps = {
-                center:centerpos,
-                navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                zoom: 15
-            };
+                            var optionsGmaps = {
+                                center:centerpos,
+                                mapTypeControlOptions:{
+                                    style:google.maps.MapTypeControlStyle.DROPDOWN_MENU
+                                },
+                                streetViewControl:false,
+                                mapTypeId:google.maps.MapTypeId.ROADMAP,
+                                zoom:15
+                            };
 
 // Initialisation de la carte avec les options
-            var map = new google.maps.Map(document.getElementById("map"), optionsGmaps);
+                            var map = new google.maps.Map(document.getElementById("map"), optionsGmaps);
 
-            if(navigator.geolocation) {
+                            if (navigator.geolocation) {
 
-                // Fonction de callback en cas de succès
-                function affichePosition(position) {
+                                // Fonction de callback en cas de succès
+                                function affichePosition(position) {
+                                    codeLatLng(position.coords.latitude, position.coords.longitude);
+                                    // On instancie un nouvel objet LatLng pour Google Maps
+                                    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-                    // On instancie un nouvel objet LatLng pour Google Maps
-                    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                                    // Ajout d'un marqueur à la position trouvée
 
-                    // Ajout d'un marqueur à la position trouvée
-                    var marker = new google.maps.Marker({
-                        position: latlng,
-                        map: map,
-                        title:"Vous êtes ici"
-                    });
+                                    map.panTo(latlng);
 
-                    map.panTo(latlng);
+                                }
+
+                                navigator.geolocation.getCurrentPosition(affichePosition, erreurPosition);
+
+                            } else {
+
+                                console.log("Ce navigateur ne supporte pas la géolocalisation");
+
+                            }
+
+
+                        }
+                    }
+                    return app;
 
                 }
-
-              navigator.geolocation.getCurrentPosition(affichePosition,erreurPosition);
-
-            } else {
-
-                console.log("Ce navigateur ne supporte pas la géolocalisation");
-
-            }
-
-
-
-            }
-    }
-    return app;
-
-});
+            )
+                ;
--- public/javascripts/routes.js
+++ public/javascripts/routes.js
@@ -7,100 +7,130 @@ define([
     'model/user'
 ], function (homeC, messageC, eventC, menuC, functionH, user) {
 
+        //vue qui n a pas besoin d etre logger et c la meme on et offline
+        var loadView = function (view, next) {
+            $.get('templates/menu-nonlogged.html', function (data) {
+                $('#menu').html(data);
+                menuC.nonlogged();
+            }, 'html');
+            ko.removeNode($('#into'));
+            $.get('templates/' + view + '.html', function (data) {
+                $('#into').html(data);
+                next();
+            }, 'html');
+        }
 
-    var loadView = function (view, next) {
-        $.get('templates/menu-nonlogged.html', function (data) {
-            $('#menu').html(data);
-            menuC.nonlogged();
-        }, 'html');
-        ko.removeNode($('#into'));
-        $.get('templates/' + view + '.html', function (data) {
-            $('#into').html(data);
-            next();
-        }, 'html');
-    }
 
-    var loadViewOnOffline=function(view,next){
-        $.get('templates/menu-nonlogged.html', function (data) {
-            $('#menu').html(data);
-            menuC.nonlogged();
-        }, 'html');
-        ko.removeNode($('#into'));
-        $.get('templates/' + view + '.html', function (data) {
-            $('#into').html(data);
-            next();
-        }, 'html');
-    }
-
-    var loadViewLogged = function (view, next) {
-        user.isLogged(function (logged) {
-            if (logged) {
-                $.get('templates/menu-logged.html', function (data) {
-                    $('#menu').html(data);
-                    menuC.logged();
-                }, 'html');
-                ko.removeNode($('#into'));
-                $.get('templates/' + view + '.html', function (data) {
-                    $('#into').html(data);
-                    next();
-                }, 'html');
-            } else {
-                window.location.hash='';
-            }
-        });
-    }
-
-    var AppRouter = Backbone.Router.extend({
-        routes:{
-            "inscription":"home",
-            "message":"message",
-            "event":"event",
-            "*actions":"defaultRoute" // Backbone will try match the route above first
-        },
-        home:function () {
+        //vue quand on a pas logger et qui affiche ou une vue online ou une vue offline
+        var loadViewOnOffline = function (view, next) {
+            $.get('templates/menu-nonlogged.html', function (data) {
+                $('#menu').html(data);
+                menuC.nonlogged();
+            }, 'html');
+            ko.removeNode($('#into'));
             functionH.isConnected(function (isConnected) {
                 if (isConnected) {
-                    loadView('home', function () {
-                        homeC.init();
-                    });
+                    $.get('templates/online/' + view + '.html', function (data) {
+                        $('#into').html(data);
+                        next({online:true});
+                    }, 'html');
                 } else {
-                    loadView('home-offline', function () {
-                    });
+                    $.get('templates/offline/' + view + '.html', function (data) {
+                        $('#into').html(data);
+                        next({online:false});
+                    }, 'html');
                 }
             });
+        }
+        //vue quand on a pas logger meme vue on ou offline
+        var loadViewLogged = function (view, next) {
+            user.isLogged(function (logged) {
+                if (logged) {
+                    $.get('templates/menu-logged.html', function (data) {
+                        $('#menu').html(data);
+                        menuC.logged();
+                    }, 'html');
+                    ko.removeNode($('#into'));
+                    $.get('templates/' + view + '.html', function (data) {
+                        $('#into').html(data);
+                        next();
+                    }, 'html');
+                } else {
+                    window.location.hash = '';
+                }
+            });
+        }
 
-        },
-        message:function () {
-                    loadViewLogged('message', function () {
-                        messageC.init();
+        //vue logger et qui affiche ou une vue online ou une vue offline
+        var loadViewloggerOnOffline = function (view, next) {
+            user.isLogged(function (logged) {
+                if (logged) {
+                    $.get('templates/menu-logged.html', function (data) {
+                        $('#menu').html(data);
+                        menuC.nonlogged();
+                    }, 'html');
+                    ko.removeNode($('#into'));
+                    functionH.isConnected(function (isConnected) {
+                        if (isConnected) {
+                            $.get('templates/online/' + view + '.html', function (data) {
+                                $('#into').html(data);
+                                next({online:true});
+                            }, 'html');
+                        } else {
+                            $.get('templates/offline/' + view + '.html', function (data) {
+                                $('#into').html(data);
+                                next({online:false});
+                            }, 'html');
+                        }
                     });
-        },
-        event:function () {
+                } else {
+                    window.location.hash = '';
+                }
+            });
+        }
+
+        var AppRouter = Backbone.Router.extend({
+            routes:{
+                "inscription":"home",
+                "message":"message",
+                "event":"event",
+                "*actions":"defaultRoute" // Backbone will try match the route above first
+            },
+            home:function () {
+                loadViewloggerOnOffline('home', function (online) {
+                    if (online) {
+                        homeC.init();
+                    }
+                });
+            },
+            message:function () {
+                loadViewLogged('message', function () {
+                    messageC.init();
+                });
+            },
+            event:function () {
                 loadViewLogged('event', function () {
                     eventC.init();
                 });
-        },
-        defaultRoute:function () {
-            functionH.isConnected(function (isConnected) {
-                if (isConnected) {
-                    loadView('home', function () {
+            },
+            defaultRoute:function () {
+                loadViewOnOffline('home', function (online) {
+                    if (online) {
                         homeC.init();
-                    });
-                } else {
-                    loadView('home-offline', function () {
-                    });
-                }
-            });
-        }
-    });
+                    }
+                });
+            }
+        });
 
 
-    var app = {
-        init:function () {
-            var app_router = new AppRouter;
-            Backbone.history.start();
+        var app = {
+            init:function () {
+                var app_router = new AppRouter;
+                Backbone.history.start();
+            }
         }
+        return app;
+
     }
-    return app;
-
-});
+)
+;
