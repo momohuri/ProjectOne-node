a278a6aeef240f53c6047dea6da83ed4a503a177
Maurin <maurin.lenglart@gmail.com>
2012-11-04 23:47:30 +0100

    @

--- public/javascripts/controller/home.js
+++ public/javascripts/controller/home.js
@@ -4,23 +4,11 @@ define([
     "extern/bootstrap.min"
 ], function (user) {
 
-    function get_date(text, user) {
-        var d_split = text.split('/');
-        var d = new Date(0);
-        return d.setFullYear(d_split[2], d_split[1], d_split[0])
-    }
-
     function datepicker() {
-        $('#start, #end').datepicker().on('changeDate', function (e) {
-            if (get_date($('#start').val()) >= get_date($('#end').val())) {
-                $('#alerte').show();
-                $('#Search').attr('disabled', true);
-            }
-            else {
-                $('#alerte').hide();
-                $('#Search').attr('disabled', false);
-            }
-        });
+        $('#start').datepicker();
+        var today = new Date();
+        var t = today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear().toString().substr(2,4);
+        $('#start').val(t);
     }
 
     var app = {
