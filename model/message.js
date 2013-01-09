231b61396c1ad157dd0bec7ace9b656a15453e51
AdrienVinches <adrien.vinches@supinfo.com>
2013-01-08 16:27:45 -0800

    test

--- model/message.js
+++ model/message.js
@@ -9,7 +9,7 @@ if (typeof define !== 'function') {
         "./user"
     ], function (Sequelize,user) {
         if(!global.sequelize){
-            var sequelize= global.sequelize = new Sequelize("projectone", "root");
+            var sequelize= global.sequelize = new Sequelize("projectone", "root", "root");
         }else{
             var sequelize= global.sequelize;
         }
